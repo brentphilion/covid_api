9731b872bea562eb2613dece3e8f8e4b941dfccd
brentphilion <43643086+brentphilion@users.noreply.github.com>
2020-10-15 17:07:58 -0400

    Starting to format the thingie

---
+++ api_call.py
@@ -0,0 +1,19 @@
+"""
+Generates API call to covid project to try and parse out active cases in cnaada
+"""
+import csv
+import requests
+import json
+from dataclasses import dataclass
+
+def main():
+    """
+    Entrypoint into program.
+    """
+
+
+
+
+
+if __name__ == '__main__':
+    main()
