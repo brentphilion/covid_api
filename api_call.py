5cd5c1400714c85d8b2db59f8e2b93c570e423a9
brentphilion <43643086+brentphilion@users.noreply.github.com>
2020-10-16 08:27:43 -0400

    call done

--- api_call.py
+++ api_call.py
@@ -1,18 +1,30 @@
 """
-Generates API call to covid project to try and parse out active cases in cnaada
+Generates API call to covid project to try and parse out active cases in canada
 """
-import csv
 import requests
 import json
-from dataclasses import dataclass
+
 
 def main():
     """
     Entrypoint into program.
     """
 
+    call_api()
 
 
+def call_api():
+    """
+    call https://services9.arcgis.com/pJENMVYPQqZZe20v/arcgis/rest/services/Canada_COVID19_Case_Details/FeatureServer/0/query?where=1%3D1&outFields=row_id,date_reported,age_group,gender,province,case_status,health_region&outSR=4326&f=json
+    Read the json and turn it into a CSV or pandas dataframe
+    """
+
+    api_url = "https://services9.arcgis.com/pJENMVYPQqZZe20v/arcgis/rest/services/Canada_COVID19_Case_Details/FeatureServer/0/"
+    api_argument = "query?where=1%3D1&outFields=row_id,date_reported,age_group,gender,province,case_status,health_region&outSR=4326&f=json"
+    response = requests.get(f'{api_url}{api_argument}')
+    web_data = response.json()  # returns a list of dicts
+    print(type(web_data))
+    print(web_data)
 
 
 if __name__ == '__main__':
