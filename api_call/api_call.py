ac39dc00a3c0d98aaba5c4afb5c2198deb7897c5
brentphilion <43643086+brentphilion@users.noreply.github.com>
2020-11-07 08:03:47 -0500

    Starting to try and read the data into Pandas... not working well

--- api_call/api_call.py
+++ api_call/api_call.py
@@ -2,8 +2,7 @@
 Generates API call to covid project to try and parse out active cases in canada
 """
 import requests
-import json
-import pandas
+import pandas as pd
 import csv
 
 
@@ -14,7 +13,9 @@ def main():
     """
     web_data= call_api()
 
-    save_data(web_data)
+    pandas_parse(web_data)
+
+#    save_csv(web_data)
 
 """
     parse_data(web_data)
@@ -30,17 +31,22 @@ def call_api():
     api_argument = "query?where=1%3D1&outFields=row_id,date_reported,age_group,gender,province,case_status,health_region&outSR=4326&f=json"
     response = requests.get(f'{api_url}{api_argument}')
     web_data = response.json()  # returns a list of dicts
-    print(type(web_data))
-    return (web_data)
+
+    return (web_data, response)
 
 def parse_data():
     pass
 
-def save_data(web_data):
+def save_csv(web_data):
+    csv_fields = {'row_id','date_reported','age_group','gender','province','case_status','health_region'}
     with open('cases.csv', 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames = {} )
+        writer = csv.DictWriter(csvfile, fieldnames=csv_fields)
         writer.writeheader()
         writer.writerows(web_data)
+
+def pandas_parse(response):
+    cases = pd.read_json(response)
+    "df.to_csv(index=False)"
 
 """
 code i stole
@@ -48,6 +54,7 @@ with open('Names.csv', 'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=field_names)
     writer.writeheader()
     writer.writerows(cars)
+    Fields=row_id,date_reported,age_group,gender,province,case_status,health_region
  """
 
 if __name__ == '__main__':
